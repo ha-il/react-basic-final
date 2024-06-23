5fcda699752de440c4b7a8ddc55b9c34affb4431
Hyeongwoo Kim <hyeongwookim.dev@gmail.com>
2024-06-23 13:31:31 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+);
+
+export default App;
